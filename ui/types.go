9d1e2023660eb9ba71b38f3877ce6ba859eedc4e
Prime-Cave <tomisin.akinfemiwa@gmail.com>
2024-01-15 12:43:18 +0100

    Initial commit

---
+++ ui/types.go
@@ -0,0 +1,17 @@
+package ui
+
+import (
+	"pixl/apptype"
+	"pixl/pxcanvas"
+	"pixl/swatch"
+
+	"fyne.io/fyne/v2"
+)
+
+type AppInit struct {
+	PixlCanvas *pxcanvas.PxCanvas
+	PixlWindow fyne.Window
+	State *apptype.State
+	Swatches []*swatch.Swatch
+}
+
